a96b965018d7b69a3574b6e8549f331bd4629593
ZahirulNiloyCodes <zcodes249@gmail.com>
2025-12-20 01:00:39 +0600

    server crash fixed

--- server/routes.js
+++ server/routes.js
@@ -1,4 +1,7 @@
-const client = require("./db");
+const client = require("./supabase");
 const express = require("express");
 const router = express.Router();
+router.get("/", (req, res) => {
+  res.end("Heloo");
+});
 module.exports = router;
